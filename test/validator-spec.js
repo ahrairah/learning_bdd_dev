eda5e4da85e50648385a3c5f2a44c204b4266690
Tony Meyer <tony.meyer@sage.com>
2016-05-27 14:16:04 +0200

    Chai und Mocha in Betrieb genommen.

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,7 +1,12 @@
 var assert = require('assert');
+var chai = require('chai');
+var  expect = chai.expect;
+var should = chai.should();
+validator = require('../lib/validator');
 
-describe('Be welcome to Mocha', function(){
-	it('with a failing test', function(){
-		assert(false, 'Hello World');
+
+describe('A Validator', function(){
+	it('will return error.nonpositive for not strictly postitive numbers', function(){
+     		expect(validator(0).should.be.deep.equal(['error.nonpositive']));
 	});
 });
