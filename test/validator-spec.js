1d5bfaf72cd26e700a54ff438eb5de511598710e
Tony Meyer <tony.meyer@sage.com>
2016-05-30 09:10:21 +0200

    Further testing.

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -6,7 +6,15 @@ validator = require('../lib/validator');
 
 
 describe('A Validator', function(){
-	it('will return error.nonpositive for not strictly postitive numbers', function(){
-     		expect(validator(0).should.be.deep.equal(['error.nonpositive']));
+	it('will return no errors for valid numbers', function(){
+		expect(validator(3)).to.be.empty;
+	});
+
+	it('will return error.nonpositive for not strictly postitive numbers, like 0', function(){
+     		expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+	});
+	
+	it('will return error.nonpositive for not strictly positive numbers, like -2', function(){
+		expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
 	});
 });
