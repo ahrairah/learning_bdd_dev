d6e5ce9511e972e1c332afe4a8162ad18fbc3d6f
Tony Meyer <Tony.Meyer@outlook.de>
2016-05-30 09:47:36 +0200

    changed tests to include several errors.

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -9,32 +9,35 @@ describe('A Validator', function () {
     it('will return no errors for valid numbers', function () {
         expect(validator(7)).to.be.empty;
     });
-    it('will return one error for each rule the number violates', function () {
-        expect(validator(15)).to.be.deep.equal(['error.three', 'error.five']);
-    });
-    describe('will return error.nonpositive for not strictly positive numbers:', function () {
+    describe('will include error.nonpositive for not strictly positive numbers:', function () {
         it('like 0', function () {
-            expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+            expect(validator(0)).to.include(['error.nonpositive']);
         });
 
         it('like -2', function () {
-            expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+            expect(validator(-2)).to.include(['error.nonpositive']);
         });
     });
-    describe('will return error.three for divisible by 3 numbers:', function () {
+    describe('will include error.three for divisible by 3 numbers:', function () {
         it('like 3', function () {
             expect(validator(3)).to.be.deep.equal(['error.three']);
         });
         it('like 6', function () {
             expect(validator(6)).to.be.deep.equal(['error.three']);
         });
+        it('like 15', function () {
+            expect(validator(15)).to.include(['error.three']);
+        });
     });
-    describe('will return error.five for divisible by 5 numbers:', function () {
+    describe('will include error.five for divisible by 5 numbers:', function () {
         it('like 5', function () {
             expect(validator(5)).to.be.deep.equal(['error.five']);
         });
         it('like 10', function () {
             expect(validator(10)).to.be.deep.equal(['error.five']);
+        });
+        it('like 15', function () {
+            expect(validator(15)).to.inlude(['error.five']);
         });
     });
 
