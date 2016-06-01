ff44a929a782d196d6dc4f0a7317d241fefb4786
Tony Meyer <Tony.Meyer@outlook.de>
2016-06-01 12:57:59 +0200

    added pendig test snippets

---
+++ myCafe/test/step_definitions/display_order_steps.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports = function () {
+  this.Given(/^that the order is empty$/, function (cb) {
+    cb.pending();
+  });
+  this.When(/^the customer displays the order$/, function (cb) {
+    cb.pending();
+  });
+  this.Then(/^no order items will be displayed$/, function (cb) {
+    cb.pending();
+  });
+  this.Then(/^"([^"]*)" will be shown as total price$/, function (expectedTotalPrice, cb) {
+    cb.pending();
+  });
+  this.Then(/^there will only be the possibility to add a beverage$/, function (cb) {
+    cb.pending();
+  })
+};
