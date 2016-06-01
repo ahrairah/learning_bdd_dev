87914f3f85516ff5008040561d0e596ce364e1a4
Tony Meyer <tony.meyer@sage.com>
2016-06-01 13:31:35 +0200

    fixed test steps

--- myCafe/features/making_an_order/customer_displays_order.feature
+++ myCafe/features/making_an_order/customer_displays_order.feature
@@ -7,8 +7,8 @@ Feature: Customer displays order
 	in order to review the contents of my order and its price easily
 
 	Scenario: Order is empty
-    Given that the order is empty
-    When the customer displays the order
-    Then no order items will be shown
-    "0" will be shown as total price
-    there will only be the possibility to add a beverage
+        Given that the order is empty
+        When the customer displays the order
+        Then no order items will be shown
+        And "0" will be shown as total price
+        And there will only be the possibility to add a beverage
--- myCafe/test/step_definitions/display_order_steps.js
+++ myCafe/test/step_definitions/display_order_steps.js
@@ -1,19 +1,19 @@
 'use strict'
 
 module.exports = function () {
-  this.Given(/^that the order is empty$/, function (callback) {
-    callback(null, 'pending');
+  this.Given(/^that the order is empty$/, function (cb) {
+    cb(null, 'pending');
   });
   this.When(/^the customer displays the order$/, function (cb) {
-    cb.pending();
+    cb(null, 'pending');
   });
   this.Then(/^no order items will be shown$/, function (cb) {
-    cb.pending();
+    cb(unll, 'pending');
   });
   this.Then(/^"([^"]*)" will be shown as total price$/, function (expectedTotalPrice, cb) {
-    cb.pending();
+    cb(null, 'pending');
   });
   this.Then(/^there will only be the possibility to add a beverage$/, function (cb) {
-    cb.pending();
+    cb(null, 'pending');
   });
 };
